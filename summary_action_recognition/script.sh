fe2f940a3cd677cd3b5afbbb441a829770a5b885
Swati640 <45712497+Swati640@users.noreply.github.com>
2020-08-04 17:40:00 +0200

    Delete script.sh

--- summary_action_recognition/script.sh
+++
@@ -1,4 +0,0 @@
-python create_split.py  --save-dir datasets  --save-name splits --num-splits 5 --train-percent 0.8
-python summary2video.py --path summe-split1/result.h5  --frm-dir ApplyEyeMakeup -i 1 --fps 7 --width 640 --height 480
-python train.py --dataset_path data/UCF-101-frame
-python main.py --dataset datasets/UCF101.h5 -s datasets/UCF101.json --gpu 0
